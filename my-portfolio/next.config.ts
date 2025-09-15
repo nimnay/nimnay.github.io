1143c8072511e58d32530da97301266f79bafa2c
Nimra Nayyar <nimranayyar05@gmail.com>
2025-09-15 16:51:07 -0400

    update

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
-  distDir: 'out',
   images: {
     unoptimized: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   }
 };
 
